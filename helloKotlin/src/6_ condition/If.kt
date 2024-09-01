6c5ef69b16adf55c5fbdd12b3d58da2d862365f7
나병현 <nbhyun0329@gmail.com>
2024-09-02 08:23:23 +0900

    2024-09-02

---
+++ helloKotlin/src/6_ condition/If.kt
@@ -0,0 +1,12 @@
+package `6_ condition`
+
+fun main() {
+
+    var a = 11
+
+    if(a > 10) {
+        println("a is greater than 10") // 조건문이 참이면 실행될 로직
+    } else {
+        println("a is greater than or equal to 10") // 조건문이 거짓일때 실행될 로직
+    }
+}
